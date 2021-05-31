5f5dbc3e22022e1203e922492ca184322ea0330a
elena_kau <lelickau@yandex.ru>
2021-05-31 16:10:53 +0200

    modal calc

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,13 +1,9 @@
-const forms = () => {
+import checkNumInputs from './checkNumInputs';
+const forms = (state) => {
     const form = document.querySelectorAll('form'),
-        inputs = document.querySelectorAll('input'),
-        phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+        inputs = document.querySelectorAll('input');
 
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        })
-    })
+    checkNumInputs('input[name="user_phone"]');
 
     const message = {
         loading: 'Загрузка...',
@@ -39,6 +35,13 @@ const forms = () => {
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
+
+            if (item.getAttribute('data-calc') === 'end') {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
+
             postData('assets/server.php', formData)
                 .then(res => {
                     console.log(res);
